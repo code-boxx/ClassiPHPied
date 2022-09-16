d4a996ec9c3bd5dee8b380d72948fb9996b0a9bc
Code Boxx <code-boxx@mail.com>
2022-09-16 16:04:40 +0800

    16 Sep 2022
    1) Installer cleanup.
    2) Small LIB-Route changes
    3) Added dummy image into uploads folder.

--- pages/ADM-check.php
+++ pages/ADM-check.php
@@ -6,7 +6,7 @@ if (!isset($_SESS["user"]) && $_PATH!="admin/login/") {
 }
 
 // (B) LOAD REQUESTED PAGE
-$_PATH = explode("/", rtrim($_PATH, "/"));
+$_PATH = explode("/", rtrim($_PATH, "/\\"));
 array_shift($_PATH);
 $_CORE->Route->load(
   count($_PATH)==0 ? "ADM-home.php" : "ADM-" . implode("-", $_PATH) . ".php"
