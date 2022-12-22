f2c0ff243b07abccb09fbe23a90a42c294ad6e00
Code Boxx <code-boxx@mail.com>
2022-12-22 13:28:40 +0800

    Update CB-worker.js

--- CB-worker.js
+++ CB-worker.js
@@ -1,39 +1,41 @@
-// (A) FILES TO CACHE
-const cName = "classiphpied",
-cFiles = [
-  // (A1) BOOTSTRAP
-  "assets/bootstrap.bundle.min.js",
-  "assets/bootstrap.bundle.min.js.map",
-  "assets/bootstrap.min.css",
-  "assets/bootstrap.min.css.map",
-  // (A2) ICONS + IMAGES
-  "assets/favicon.png",
-  "assets/ico-512.png",
-  // (A3) COMMON INTERFACE
-  "CB-manifest.json",
-  "assets/PAGE-cb.js",
-  "assets/maticon.woff2",
-  // (A4) PAGES
-  "assets/ADM-category.js",
-  "assets/ADM-classified.js",
-  "ADM-classified-images.js",
-  "assets/ADM-images.css",
-  "assets/ADM-images.js",
-  "assets/ADM-login.js",
-  "assets/ADM-settings.js",
-  "assets/ADM-users.js",
-  "assets/PAGE-classified.css",
-  "assets/PAGE-classified.js",
-  "assets/PAGE-forgot.js",
-  "assets/PAGE-show.js"
-];
+// (A) CREATE/INSTALL CACHE
+self.addEventListener("install", evt => {
+  self.skipWaiting();
+  evt.waitUntil(
+    caches.open("classiphpied")
+    .then(cache => cache.addAll([
+      // (A1) BOOTSTRAP
+      "assets/bootstrap.bundle.min.js",
+      "assets/bootstrap.bundle.min.js.map",
+      "assets/bootstrap.min.css",
+      "assets/bootstrap.min.css.map",
+      // (A2) ICONS + IMAGES
+      "assets/favicon.png",
+      "assets/ico-512.png",
+      // (A3) COMMON INTERFACE
+      "CB-manifest.json",
+      "assets/PAGE-cb.js",
+      "assets/maticon.woff2",
+      // (A4) PAGES
+      "assets/ADM-category.js",
+      "assets/ADM-classified.js",
+      "ADM-classified-images.js",
+      "assets/ADM-images.css",
+      "assets/ADM-images.js",
+      "assets/ADM-login.js",
+      "assets/ADM-settings.js",
+      "assets/ADM-users.js",
+      "assets/PAGE-classified.css",
+      "assets/PAGE-classified.js",
+      "assets/PAGE-forgot.js",
+      "assets/PAGE-show.js"
+    ]))
+    .catch(err => console.error(err))
+  );
+});
 
-// (B) CREATE/INSTALL CACHE
-self.addEventListener("install", evt => evt.waitUntil(
-  caches.open(cName)
-  .then(cache => cache.addAll(cFiles))
-  .catch(err => console.error(err))
-));
+// (B) CLAIM CONTROL INSTANTLY
+self.addEventListener("activate", evt => self.clients.claim());
 
 // (C) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
 self.addEventListener("fetch", evt => evt.respondWith(
